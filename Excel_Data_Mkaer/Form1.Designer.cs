f01c302e24abf128a26df0de07af9a9d78f553ac
a.Hosseinzadeh <a.Hosseinzadeh@D8080200C5>
2020-10-16 00:57:52 +0330

    Init

---
+++ Excel_Data_Mkaer/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Excel_Data_Mkaer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseDocxBtn = new System.Windows.Forms.Button();
+            this.filePathTxt = new System.Windows.Forms.TextBox();
+            this.dataGridDocx = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridDocx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chooseDocxBtn
+            // 
+            this.chooseDocxBtn.Location = new System.Drawing.Point(499, 69);
+            this.chooseDocxBtn.Name = "chooseDocxBtn";
+            this.chooseDocxBtn.Size = new System.Drawing.Size(75, 23);
+            this.chooseDocxBtn.TabIndex = 0;
+            this.chooseDocxBtn.Text = "...";
+            this.chooseDocxBtn.UseVisualStyleBackColor = true;
+            this.chooseDocxBtn.Click += new System.EventHandler(this.chooseDocxBtn_Click);
+            // 
+            // filePathTxt
+            // 
+            this.filePathTxt.Location = new System.Drawing.Point(78, 69);
+            this.filePathTxt.Name = "filePathTxt";
+            this.filePathTxt.Size = new System.Drawing.Size(394, 20);
+            this.filePathTxt.TabIndex = 1;
+            // 
+            // dataGridDocx
+            // 
+            this.dataGridDocx.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridDocx.Location = new System.Drawing.Point(12, 110);
+            this.dataGridDocx.Name = "dataGridDocx";
+            this.dataGridDocx.Size = new System.Drawing.Size(633, 291);
+            this.dataGridDocx.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 450);
+            this.Controls.Add(this.dataGridDocx);
+            this.Controls.Add(this.filePathTxt);
+            this.Controls.Add(this.chooseDocxBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridDocx)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chooseDocxBtn;
+        private System.Windows.Forms.TextBox filePathTxt;
+        private System.Windows.Forms.DataGridView dataGridDocx;
+    }
+}
+
---
+++ Excel_Data_Mkaer/Form1.cs
@@ -0,0 +1,43 @@
+﻿using ExcelDataReader;
+using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Excel_Data_Mkaer
+{
+    public partial class Form1:Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DataTableCollection dataTableCollection;
+
+        private void chooseDocxBtn_Click(object sender,EventArgs e)
+        {
+            using(var openIFileDialog = new OpenFileDialog() { Filter = "format 1|*.xls|format 2|*.xlsx" })
+            {
+                if(openIFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePathTxt.Text = openIFileDialog.FileName;
+
+                    using(var stream = File.Open(openIFileDialog.FileName,FileMode.Open,FileAccess.Read))
+                    {
+                        using(IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            DataSet result = reader.AsDataSet(configuration: new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable = (_) => new ExcelDataTableConfiguration { UseHeaderRow = true }
+                            });
+                            dataTableCollection = result.Tables;
+                            DataTable dt = dataTableCollection[0];
+                            dataGridDocx.DataSource = dt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
